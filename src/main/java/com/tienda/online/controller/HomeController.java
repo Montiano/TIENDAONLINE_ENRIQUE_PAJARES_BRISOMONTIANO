96416493608a2cf8ccdbe1e32c1efd506b577f1a
Montiano <kikemontiano17@gmail.com>
2022-05-08 10:50:15 +0200

    Update HomeController.java

--- src/main/java/com/tienda/online/controller/HomeController.java
+++ src/main/java/com/tienda/online/controller/HomeController.java
@@ -18,7 +18,9 @@ import org.springframework.web.bind.annotation.RequestParam;
 import com.tienda.online.model.DetallePedido;
 import com.tienda.online.model.Pedido;
 import com.tienda.online.model.Producto;
-import com.tienda.online.service.ProductoService;
+import com.tienda.online.model.Usuario;
+import com.tienda.online.service.IProductoService;
+import com.tienda.online.service.IUsuarioService;
 
 @Controller
 @RequestMapping("/")
@@ -27,7 +29,10 @@ public class HomeController {
 	private final Logger LOGGER = LoggerFactory.getLogger(HomeController.class);
 	
 	@Autowired
-	private ProductoService productoService;
+	private IProductoService productoService;
+	
+	@Autowired
+	private IUsuarioService usuarioService;
 	
 	List<DetallePedido> detallesPedido = new ArrayList<DetallePedido>();
 	
@@ -62,14 +67,41 @@ public class HomeController {
 		LOGGER.info("Unidades: {}", unidades);
 		producto = productoOptional.get();
 		
+//		int posicion = 0;
+//		if(detallesPedido.size()>0) {
+//			for (int i = 0; i < detallesPedido.size(); i++) {
+//				if(id == detallesPedido.get(i).getId()) {
+//					posicion = i;
+//				}
+//			}
+//			if (id == detallesPedido.get(posicion).getId()) {
+//				unidades = detallesPedido.get(posicion).getUnidades() + unidades;
+//				sumaTotal = detallesPedido.get(posicion).getPrecioUnidad() * unidades;
+//				detallesPedido.get(posicion).setUnidades(unidades);
+//				detallesPedido.get(posicion).setTotal(sumaTotal);
+//			} else {
+//				detallesPedido.add(detallePedido);
+//			}
+//		} else {
+//			detallesPedido.add(detallePedido);
+//		}
+		
 		detallePedido.setUnidades(unidades);
 		detallePedido.setPrecioUnidad(producto.getPrecio());
 		detallePedido.setNombre(producto.getNombre());
 		detallePedido.setTotal(producto.getPrecio()*unidades);
 		detallePedido.setProducto(producto);
 		
-		detallesPedido.add(detallePedido);
-		
+		// Validar para que solo haya un producto por lista y no se añada dos veces
+		Long idProducto = producto.getId();
+		// Función anónima con la que recorremos toda la lista y si coinciden los ids ya existe
+		boolean existe = detallesPedido.stream().anyMatch(p->p.getProducto().getId()==idProducto);
+		// Si no existe lo añade, si no, no
+		if(!existe) {
+			detallesPedido.add(detallePedido);
+		}
+			
+		// Función anónima con la que cogemos todos los productos y los sumamos
 		sumaTotal = detallesPedido.stream().mapToDouble(dt->dt.getTotal()).sum();
 		
 		pedido.setTotal(sumaTotal);
@@ -103,4 +135,28 @@ public class HomeController {
 		
 		return "usuario/carrito";
 	}
+	
+	@GetMapping("/getCart")
+	public String getCart(Model modelo) {
+		
+		modelo.addAttribute("detallesPedido", detallesPedido);
+		modelo.addAttribute("pedido", pedido);
+		
+		return "/usuario/carrito";
+	}
+	
+	@GetMapping("/order")
+	public String order(Model modelo) {	
+		Usuario usuario = usuarioService.findById(2L).get();
+		
+		modelo.addAttribute("detallesPedido", detallesPedido);
+		modelo.addAttribute("pedido", pedido);
+		modelo.addAttribute("usuario", usuario);
+		
+		return "usuario/resumen_pedido";
+	}
+	
+	
+	
+	
 }
