883678503ae255e02898a345cb957d974f9040cf
Montiano <kikemontiano17@gmail.com>
2022-05-08 10:50:29 +0200

    Create IProductoService.java

---
+++ src/main/java/com/tienda/online/service/IProductoService.java
@@ -0,0 +1,20 @@
+package com.tienda.online.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import com.tienda.online.model.Producto;
+
+public interface IProductoService {
+
+	public Producto save(Producto producto);	
+	// Con optional podemos validar si el objeto existe o no en BD
+	public Optional<Producto> get(Long id);
+	
+	public void update(Producto producto);
+	
+	public void delete(Long id);
+	
+	public List<Producto> findAll();
+	
+}
