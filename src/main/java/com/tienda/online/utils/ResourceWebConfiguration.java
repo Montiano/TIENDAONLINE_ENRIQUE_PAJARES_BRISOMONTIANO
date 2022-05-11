0a9e85263f8bd20d95363d37a44d4e35a2202b0c
Montiano <kikemontiano17@gmail.com>
2022-05-11 09:31:50 +0200

    ok

--- src/main/java/com/tienda/online/utils/ResourceWebConfiguration.java
+++ src/main/java/com/tienda/online/utils/ResourceWebConfiguration.java
@@ -1,4 +1,4 @@
-package com.tienda.online;
+package com.tienda.online.utils;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
